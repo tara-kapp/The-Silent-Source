0029254e0e988d601d22216e9ff17fa5c1bdddb3
Arin <kaplanae@g.cofc.edu>
2024-11-14 12:06:55 -0500

    Ui update

--- CustomPixelProject/Assets/spriteSwitch.cs
+++ CustomPixelProject/Assets/spriteSwitch.cs
@@ -7,17 +7,38 @@ using TMPro;
 public class spriteSwitch : MonoBehaviour
 {
     public Texture[] spriteArray;
-    public int spriteNum = 0;
+    public int spriteNum;
     public RawImage sprite;
     public TMP_Text messageText;
     public string[] stringArray;
 
+    public GameObject gameobject;
+    public GameObject[] products;
+
     // Update is called once per frame
-    void Update()
-    {
-        UpdateSprite();
+
+    void Start(){
+        gameobject.SetActive(false);
+        spriteNum = 0;
     }
 
+    void Update(){
+        
+    }
+
+    public void OnClick(){
+        products[spriteNum].SetActive(false);
+        UpdateSprite();
+        gameobject.SetActive(true);
+        
+    }
+
+    public void OnExit(){
+        gameobject.SetActive(false);
+        spriteNum++;
+        products[spriteNum].SetActive(true);
+
+    }
     void UpdateSprite() {
         sprite.texture = spriteArray[spriteNum];
         messageText.SetText(stringArray[spriteNum]);
